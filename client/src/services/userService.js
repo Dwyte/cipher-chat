62d1e856938cd366433876902d8945217ecac4a9
Dwyte <dwyte@tutanota.com>
2019-07-07 16:14:45 +0800

    last fix

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,6 +1,6 @@
 import http from "./httpService";
 
-const endpoint = "http://localhost:4200/api/users";
+const endpoint = "/api/users";
 
 export const getAllUsers = async () => {
   return await http.get(endpoint);
