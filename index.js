13b5af371c4bb918ec8b90ac073e3aec3b30c135
Dwyte <dwyte@tutanota.com>
2019-07-07 13:01:44 +0800

    Heroku Deployment

--- index.js
+++ index.js
@@ -5,18 +5,14 @@ const cors = require("cors");
 const config = require("config");
 const morgan = require("morgan");
 const socket = require("socket.io").listen(4000).sockets;
+const path = require("path");
 
 // Middlewares
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cors());
 
-// Routes
-const user = require("./routes/user");
-app.use("/api/users", user);
-
-const socketConnection = require('./socket');
-
+const socketConnection = require("./socket");
 mongoose
   .connect(config.get("MONGODB_URI"), { useNewUrlParser: true }, (err, db) => {
     console.log(`Connected to MongoDB...`);
@@ -24,6 +20,16 @@ mongoose
     socket.on("connection", socketConnection);
   })
   .catch(er => console.error(er));
+
+// Routes
+const user = require("./routes/user");
+app.use("/api/users", user);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.use("*", (req, res) => res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')));
+}
 
 const port = process.env.PORT || 4200;
 app.listen(port, () => console.log(`Listening to port ${port}...`));
