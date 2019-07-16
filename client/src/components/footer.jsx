36df5afe7e18b7919e06b5624f4b8d67cee23c06
Dwyte <dwyte@tutanota.com>
2019-07-16 14:15:39 +0800

    modified footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -4,7 +4,9 @@ const Footer = () => {
   return (
     <footer className="container">
       <small>
-        CipherChat | <a href="https://github.com/Dwyte/CipherChat">About</a>
+        CipherChat | <a href="https://github.com/Dwyte/CipherChat">Source Code</a> <br />
+        <small>Global Channel (Not Encrypted, 100 messages max)</small> <br />
+        <small>Private Channel (Encrypted, 8 messages max)</small> <br />
       </small>
     </footer>
   );
